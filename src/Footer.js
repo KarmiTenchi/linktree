1611f65f1adadfced4621844cf50251701aff89a
KarmiTenchi <73952811+KarmiTenchi@users.noreply.github.com>
2022-11-02 05:39:25 +0200

    bug fix

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
   return (
     <div className="footer">
       <div className="footer-wrapper">
-        <img className="zuri" src={Zuri} />
+        <img className="zuri" src={Zuri} alt="zuri" />
         <p>HNG Internship 9 Frontend Task</p>
-        <img className="i4g" src={I4G} />
+        <img className="i4g" src={I4G} alt="ingresive" />
       </div>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -9,12 +9,12 @@ function Header() {
     <div className="header">
       <div className="icon-wrapper">
         <a id="icon_tag" href="#">
-          <img id="profile_icon" src={icon} />
-          <img id="profile_iconTwo" src={iconTwo} />
+          <img id="profile_icon" src={icon} alt="profile_icon" />
+          <img id="profile_iconTwo" src={iconTwo} alt="profile_iconTwo" />
         </a>
       </div>
       <div id="img_container">
-        <img id="profile__img" src={profile} alt="Profile image" />
+        <img id="profile__img" src={profile} alt="Profile" />
 
         <div id="img_name">Annette Black</div>
       </div>
